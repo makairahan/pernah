91cbca07825fd1953a898a09a29c6ef67960ceb2
makaira <165066715+makairahan@users.noreply.github.com>
2024-05-28 21:20:32 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-curl_output=$(curl -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}' https://eth1.lava.build/lava-referer-5773d82f-e9cb-49fb-b7af-b0e0b931fd41/)
+curl_output=$(curl -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}' https://eth1.lava.build/lava-referer-d8843cf8-0ab3-4d02-a0f5-e3381e2a4586/)
 
 
 echo "$curl_output" >> Update.tsx
